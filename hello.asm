acb34fab0f69530f5915313444729f5574bb1b19
Jason Em <jasonem@cs.ucsb.edu>
2021-01-25 15:41:59 -0800

    version 1

--- hello.asm
+++ hello.asm
@@ -3,14 +3,42 @@
 #
 #  Data Area - allocate and initialize variables
 .data
-	# TODO: Write your string definitions here
+
+greet: .asciiz "Enter an integer:\n"
+
+magic_number: .asciiz "Hello Pandemic World of 2021!\nThe magic number is "
+
+end: .asciiz "!!!\n"
 
 #Text Area (i.e. instructions)
 .text
 main:
 
-	# TODO: Write your code here
+    # ask for input
+    li $v0, 4
+    la $a0, greet
+    syscall
+
+    # get input
+    li $v0, 5
+    syscall
+    move $t0, $v0
+
+    # print middle text
+    li $v0, 4
+    la $a0, magic_number
+    syscall
+
+    # print input
+    li $v0, 1
+    move $a0, $t0
+    syscall
+
+    #print exclamation marks
+    li $v0, 4
+    la $a0, end
+    syscall
 
 exit:
-	# Exit SPIM: Write your code here!
-
+    li $v0, 10
+    syscall
